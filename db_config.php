8f350b8f0628995e86622b063a99d787e283c556
e235733@ie.u-ryukyu.ac.jp <hamzayunta17@icloud.com>
2025-10-17 01:31:17 +0900

    fix: Stop ignoring db_config.php and add debug traces

---
+++ db_config.php
@@ -0,0 +1,8 @@
+<?php
+// データベース接続情報
+// ★★★ このファイルは絶対にGitにコミットしないでください ★★★
+$db_host = 'localhost';      // データベースのホスト名
+$db_name = 'tetrocky_zentype';  // データベース名
+$db_user = 'tetrocky_gm';  // ユーザー名
+$db_pass = 'web2025games';  // パスワード
+?>
